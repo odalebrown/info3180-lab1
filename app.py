56dcf407a2a22bbc1b4be728ca23283d62ee38b4
odalebrown <odale_brown@yahoo.com>
2020-02-02 02:40:28 -0500

    created my first Flask app

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'My home page'
-
 @app.route('/about')
 def about():
-    return render-template('about.html')
+    return render_template('about.html')
+
 
 
 @app.errorhandler(404)
@@ -29,5 +29,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host ="0.0.0.0",port=8080)
-
+    app.run(debug=True, host="0.0.0.0", port=8080)
